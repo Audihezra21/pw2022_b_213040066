286973e83ecfb20354f494d80334cdef70766341
Audihezra21 <audiahezra21@gmail.com>
2022-03-04 23:28:36 +0700

    menyelesaikan pertemuan3

---
+++ Kuliah/pertemuan 3/latihan3.php
@@ -0,0 +1,21 @@
+<?php 
+// pengkondisian /percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 30;
+if($x < 20) {
+    echo "benar";
+
+}elseif ($x == 20) {
+    echo "Binggo";
+}
+
+
+else {
+    echo "salah";
+}
+    
+?>
